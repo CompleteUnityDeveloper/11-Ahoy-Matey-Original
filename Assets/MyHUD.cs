217ec238db9329211b161bda37083db5d4d43639
Ben Tristem <github@bentristem.com>
2015-12-09 11:44:56 +0000

    AM06 Replacing Network Manager HUD.mp4

---
+++ Assets/MyHUD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyHUD : MonoBehaviour {
+
+	private NetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+		networkManager = GetComponent<NetworkManager> ();
+	}
+
+	public void MyStartHost () {
+		Debug.Log ("Starting Host at " + Time.timeSinceLevelLoad);
+		networkManager.StartHost ();
+	}
+	
+
+	void OnStartHost() {
+		Debug.Log ("Host started at " + Time.timeSinceLevelLoad);
+	}
+}
