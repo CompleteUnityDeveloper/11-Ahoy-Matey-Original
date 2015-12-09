c7d4043acc12e160bf97b458a74363b33944f4a4
Ben Tristem <github@bentristem.com>
2015-12-09 21:08:59 +0000

    Make Player Movement Networked. Note Player.cs inheritance and !isLocalPlayer check

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : NetworkBehaviour {
+
+	private Vector3 inputValue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputValue.x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		inputValue.y = 0f;
+		inputValue.z = CrossPlatformInputManager.GetAxis ("Vertical");
+
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		transform.Translate (inputValue);
+	}
+}
