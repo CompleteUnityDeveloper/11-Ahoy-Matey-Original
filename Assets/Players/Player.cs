40b4096a30ffd7daa4aad2164c6473e1563be5fe
Ben Tristem <github@bentristem.com>
2015-12-09 23:31:07 +0000

    AM10 Networked Player Movement.mp4

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Player : NetworkBehaviour {
+
+	private Vector3 inputValue;
+
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer ) {
+			return;
+		}
+
+		inputValue.x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		inputValue.y = 0f;
+		inputValue.z = CrossPlatformInputManager.GetAxis ("Vertical");
+
+		transform.Translate (inputValue);
+	}
+}
