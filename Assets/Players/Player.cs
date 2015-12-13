62711e066d52d0913f1f1e9a964980b82855119d
Ben Tristem <github@bentristem.com>
2015-12-13 13:09:13 +0000

    AM12 MultiPlayer Cameras.mp4

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -19,4 +19,8 @@ public class Player : NetworkBehaviour {
 
 		transform.Translate (inputValue);
 	}
+
+	public override void OnStartLocalPlayer() {
+		GetComponentInChildren<Camera> ().enabled = true;
+	}
 }
