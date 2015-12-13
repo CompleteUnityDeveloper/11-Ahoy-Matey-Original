1151d83a1b9d5ce07fc31442f4b798a43ac0ee83
Ben Tristem <github@bentristem.com>
2015-12-13 14:38:08 +0000

    As demoed in "Network Recap & What’s Next"

---
+++ Assets/topSeaOffset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class topSeaOffset : MonoBehaviour {
+
+public float scrollSpeed = 0.5f;
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void Update() {
+		float offset = Time.time * scrollSpeed;
+		//rend.material.SetTextureOffset("_BumpMap", new Vector2(offset, 0));
+		//rend.material.SetTextureOffset("_DetailNormalMap", new Vector2(offset, 0));
+		rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+
+	}
+}
